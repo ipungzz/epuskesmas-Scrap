9d776ed44352ea6bd8726016bec0754d9879f3cf
ziper <ipung1120@gmail.com>
2024-10-23 13:14:28 +0700

    tambah filter diluar faskes

--- input.js
+++ input.js
@@ -49,10 +49,10 @@ try {
                         await page.click("#menu_pendaftaran")
                         await new Promise(resolve => setTimeout(resolve, 2000));
                         await page.click("#menu_pendaftaran_pasien")
-                        await new Promise(resolve => setTimeout(resolve, 2000));
+                        await new Promise(resolve => setTimeout(resolve, 1000));
                         console.log('[INFO] Memilih Fitur Tunda');
                         await page.click("#button_fitur_tunda")
-                        await new Promise(resolve => setTimeout(resolve, 2000));
+                        await new Promise(resolve => setTimeout(resolve, 1000));
                         await page.click("#modal > div > div > div.modal-form > div:nth-child(1) > div > div > div > span")
                         console.log('[Action] Pilih Tanggal Waktu Tunda, 5 detik berjalan');
                         await new Promise(resolve => setTimeout(resolve, 5000));
@@ -77,35 +77,39 @@ try {
                                 await new Promise(resolve => setTimeout(resolve, 1000));
                                 await page.click('#button_next');
                                 const isPopupPresent = await checkPopup(page);
-                                        if (!isPopupPresent) {
+                                    if (!isPopupPresent) {
                                         const dataPasien = await page.evaluate(() => {
-                                        const elementJK = document.querySelector('#data_pasien'); // Memilih elemen dengan ID #data_pasien
-                                        if (elementJK) {
-                                                // Mengambil teks dari elemen
-                                                const text = elementJK.innerHTML; // Menggunakan innerHTML untuk menangkap HTML termasuk <br>
-                                                const lines = text.split('<br>'); // Memisahkan setiap baris berdasarkan <br>
-                                                
-                                                // Mencari baris yang mengandung
-                                                const namaLine = lines.find(line => line.includes('Nama:'));
-                                                const jkLine = lines.find(line => line.includes('JK:'));
-                                                const lahirLine = lines.find(line => line.includes('Lahir:'));
-                                                const namaPasien = namaLine ? namaLine.split(':')[1].trim() : null;
-                                                const jenisKelamin = jkLine ? jkLine.split(':')[1].trim() : null;
-                                                const tanggalLahir = lahirLine ? lahirLine.split(':')[1].trim() : null;
-                                                return{jenisKelamin, tanggalLahir, namaPasien}
-                                        }
-                                                return null; // Kembalikan null jika tidak ditemukan
-                                });
+                                            const elementJK = document.querySelector('#data_pasien'); // Memilih elemen dengan ID #data_pasien
+                                            if (elementJK) 
+                                            {
+                                                    // Mengambil teks dari elemen
+                                                    const text = elementJK.innerHTML; // Menggunakan innerHTML untuk menangkap HTML termasuk <br>
+                                                    const lines = text.split('<br>'); // Memisahkan setiap baris berdasarkan <br>
+                                                    
+                                                    // Mencari baris yang mengandung
+                                                    const namaLine = lines.find(line => line.includes('Nama:'));
+                                                    const jkLine = lines.find(line => line.includes('JK:'));
+                                                    const lahirLine = lines.find(line => line.includes('Lahir:'));
+                                                    const namaPasien = namaLine ? namaLine.split(':')[1].trim() : null;
+                                                    const jenisKelamin = jkLine ? jkLine.split(':')[1].trim() : null;
+                                                    const tanggalLahir = lahirLine ? lahirLine.split(':')[1].trim() : null;
+                                                    return{jenisKelamin, tanggalLahir, namaPasien}
+                                            }
+                                        return null; // Kembalikan null jika tidak ditemukan
+                                    });
+
                                 const phoneNumber = await page.evaluate(() => {
                                         const inputElement = document.querySelector('#pasien_no_hp');
                                         return inputElement ? inputElement.value : null; // Mengambil nilai dari input
                                 });
+                                const statusBpjs = await page.$eval('#data_peserta_bpjs > b:nth-child(10) > span', element => element.innerText);
                                 const hasilumur = calculateAge(dataPasien.tanggalLahir);
                                 const resulttinggi = tinggi(hasilumur.toString(), dataPasien.jenisKelamin);
                                 const resultbadan = beratIdeal(resulttinggi.toString(), hasilumur.toString(), dataPasien.jenisKelamin)
-                                console.log (`[IFNO] Sedang Memproses Data Nama: ${dataPasien.namaPasien} JK: ${dataPasien.jenisKelamin} Tanggal Lahir: ${dataPasien.tanggalLahir} Umur: ${hasilumur.toString()} dengan rata rata tinggi ${resulttinggi} dan rata rata berat ${resultbadan}`);
+                                console.log (`[IFNO] Sedang Memproses Data Status: ${statusBpjs} Nama: ${dataPasien.namaPasien} JK: ${dataPasien.jenisKelamin} Tanggal Lahir: ${dataPasien.tanggalLahir} Umur: ${hasilumur.toString()} dengan rata rata tinggi ${resulttinggi} dan rata rata berat ${resultbadan}`);
                                 // Cek jenis kelamin dan jalankan fungsi yang sesuai
-                                if (dataPasien.jenisKelamin === 'Perempuan') {
+                                if(!statusBpjs === "Diluar Faskes"){
+                                    if (dataPasien.jenisKelamin === 'Perempuan') {
                                         await new Promise(resolve => setTimeout(resolve, 1000));
                                         await page.type('#tinggi_badan', resulttinggi.toString());
                                         await page.type('#berat_badan', resultbadan.toString());
@@ -115,7 +119,20 @@ try {
                                                 await page.click('#form_create > div:nth-child(3) > div:nth-child(2) > div:nth-child(5) > div > div:nth-child(2) > label');
                                                 await page.click('#form_create > div:nth-child(3) > div:nth-child(2) > div:nth-child(4) > div > div:nth-child(3) > label');
                                                 console.log('[INFO] Sedang Memasukan Nama Tenaga Medis LILIK MASUDA. AMD. KEP (Perawat)');
-                                                await page.type('#form_create > div:nth-child(3) > div:nth-child(2) > div.form-group.tenaga_medis > div > input.form-control.input-sm.ui-autocomplete-input', "LILIK MASUDA. AMD. KEP (Perawat)");
+                                                await new Promise(resolve => setTimeout(resolve, 1000));
+                                                const hiddenInputSelector = '#form_create > div:nth-child(3) > div:nth-child(2) > div.form-group.tenaga_medis > div > input.hidden.form-control.input-sm';
+                                                await page.waitForSelector(hiddenInputSelector);
+
+                                                // Mengisi nilai pada input hidden menggunakan page.evaluate
+                                                await page.evaluate((selector, value) => {
+                                                const inputElement = document.querySelector(selector);
+                                                if (inputElement) {
+                                                inputElement.value = value; // Isi nilai yang diinginkan
+                                                // Memicu event 'change' atau 'input' agar perubahan terdeteksi (jika ada listener)
+                                                inputElement.dispatchEvent(new Event('input', { bubbles: true }));
+                                                }
+                                                }, hiddenInputSelector, '0000000000001388');
+                                                await new Promise(resolve => setTimeout(resolve, 50000));
                                                 //centang bawah dan kirim
                                                 await page.click('#skriningVisual > div.alertDiv.alert-box-success > div > div > div.col-sm-4 > div > label');
                                                 await new Promise(resolve => setTimeout(resolve, 3000));
@@ -143,6 +160,7 @@ try {
                                                 await page.click('#form_create > div:nth-child(3) > div:nth-child(2) > div:nth-child(5) > div > div:nth-child(2) > label');
                                                 await page.click('#form_create > div:nth-child(3) > div:nth-child(2) > div:nth-child(4) > div > div:nth-child(3) > label');
                                                 console.log('[INFO] Sedang Memasukan Nama Tenaga Medis Siti Mariana (Bidan)');
+                                        await new Promise(resolve => setTimeout(resolve, 1000));
                                                 await page.type('#form_create > div:nth-child(3) > div:nth-child(2) > div.form-group.tenaga_medis > div > input.form-control.input-sm.ui-autocomplete-input', "Siti Mariana (Bidan)");
                                                 await page.click('#skriningVisual > div.alertDiv.alert-box-success > div > div > div.col-sm-4 > div > label');
                                                 await new Promise(resolve => setTimeout(resolve, 2000));
@@ -168,6 +186,7 @@ try {
                                         } else{
                                                 console.log('[INFO] Mendapati data pasien dibawah umur 15 Tahun');
                                                 await page.click('#form_create > div:nth-child(3) > div:nth-child(2) > div:nth-child(4) > div > div:nth-child(2) > label');
+                                                await new Promise(resolve => setTimeout(resolve, 1000));
                                                 await page.type('#form_create > div:nth-child(3) > div:nth-child(2) > div.form-group.tenaga_medis > div > input.form-control.input-sm.ui-autocomplete-input', "Siti Mariana (Bidan)");
                                                 await page.click('#skriningVisual > div.alertDiv.alert-box-success > div > div > div.col-sm-4 > div > label');
                                                 await new Promise(resolve => setTimeout(resolve, 3000));
@@ -199,6 +218,7 @@ try {
                                         await page.click('#form_create > div:nth-child(3) > div:nth-child(2) > div:nth-child(4) > div > div:nth-child(2) > label');
                                         await new Promise(resolve => setTimeout(resolve, 3000));
                                         console.log('[INFO] Sedang Memasukan Nama Tenaga Medis LILIK MASUDA. AMD. KEP (Perawat)');
+                                        await new Promise(resolve => setTimeout(resolve, 1000));
                                         await page.type('#form_create > div:nth-child(3) > div:nth-child(2) > div.form-group.tenaga_medis > div > input.form-control.input-sm.ui-autocomplete-input', "LILIK MASUDA. AMD. KEP (Perawat)");
                                         await new Promise(resolve => setTimeout(resolve, 3000));
                                         await page.click('#skriningVisual > div.alertDiv.alert-box-success > div > div > div.col-sm-4 > div > label');
@@ -222,6 +242,11 @@ try {
                                                         console.log("[INFO] Berhasil Menambahkan Data:", entry.nik, "Nama: ", entry.nama);
                                                         await page.goto(url1, {waitUntill: "networkidle2"});
                                                 }
+                                }
+                                }else{
+                                    console.log("Pasien Diluar Faskes");
+                                    await new Promise(resolve => setTimeout(resolve, 3000));
+                                    await page.goto(url1, {waitUntill: "networkidle2"});
                                 }
                         } else {
                                 console.log("[INFO] Pasien dengan No ", entry.nik, "Pasien tidak aktif");
