ebbd5c1f91b4a62cadf81f39d4004c5cf8dcd443
ziper <ipung1120@gmail.com>
2024-10-23 19:13:27 +0700

    add 405 status

--- function/script.js
+++ function/script.js
@@ -140,6 +140,30 @@ const updateJsonStatusNotFound = (nik) => {
     }
 };
 
+const updateJsonStatusRegion = (nik) => {
+    try {
+        // Membaca file JSON dari file input
+        const data = JSON.parse(fs.readFileSync(fileinput, 'utf-8'));
+
+        // Mencari data berdasarkan NIK dan memperbarui status jika ditemukan
+        const entry = data.find(item => item.nik === nik);
+        if (entry) {
+            entry.status = "405"; // Mengubah status menjadi '404' (tidak ditemukan)
+
+            // Menyimpan kembali perubahan ke file input
+            fs.writeFileSync(fileinput, JSON.stringify(data, null, 2), 'utf-8');
+            //console.log(`Data dengan NIK ${nik} berhasil diperbarui statusnya menjadi 404 di file ${fileinput}.`);
+            
+            // Menambahkan data yang di-update ke file fix.json
+            addToFileFix(entry);
+        } else {
+            //console.log(`Data dengan NIK ${nik} tidak ditemukan di file ${fileinput}.`);
+        }
+    } catch (error) {
+        console.error("Terjadi kesalahan dalam pembacaan atau penulisan file:", error);
+    }
+};
+
 // Fungsi untuk menambahkan data yang telah di-update ke file fix.json
 const addToFileFix = (updatedEntry) => {
     try {
@@ -173,4 +197,5 @@ const addToFileFix = (updatedEntry) => {
         updateJsonStatusSucces,
         updateJsonStatusNotFound,
         addToFileFix,
+        updateJsonStatusRegion,
     };
